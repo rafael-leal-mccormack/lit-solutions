3dfa900a38d29b7a3f168194bde67c4781fceca7
Raf <rleal1995@hotmail.com>
2025-03-11 20:41:02 -0400

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import Hero from './components/Hero';
-import Services from './components/Services';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+
+import { Analytics } from "@vercel/analytics/react"
+import Hero from "./components/Hero";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Projects from "./components/Projects";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Projects />
       <Contact />
       <Footer />
+      <Analytics />
     </div>
   );
 }
